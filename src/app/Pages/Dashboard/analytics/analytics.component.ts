5ef2f22de14d9b21b7baea29aee221d8a836ecf9
nayyab234 <nayyabn10@gmail.com>
2025-06-13 01:17:52 -0700

    dashboard

--- src/app/Pages/Dashboard/analytics/analytics.component.ts
+++ src/app/Pages/Dashboard/analytics/analytics.component.ts
@@ -1,386 +1,181 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { animate, style, transition, trigger } from '@angular/animations';
 import { MenuModule } from 'headlessui-angular';
 import { IconModule } from 'src/app/shared/icon/icon.module';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { CommonModule } from '@angular/common';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { HttpClient } from '@angular/common/http';
+import {
+  ApexChart,
+  ApexNonAxisChartSeries,
+  ApexPlotOptions,
+  ApexResponsive,
+  ApexAxisChartSeries,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexYAxis,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid,
+  ApexTooltip,
+  ApexLegend
+} from 'ng-apexcharts';
+import { baseURL } from '../../../environments/dev_baseURL';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries | ApexNonAxisChartSeries;
+  chart: ApexChart;
+  labels?: any;
+  xaxis?: ApexXAxis;
+  yaxis?: ApexYAxis;
+  plotOptions?: ApexPlotOptions;
+  dataLabels?: ApexDataLabels;
+  responsive?: ApexResponsive[];
+  title?: ApexTitleSubtitle;
+  colors?: any[];
+  stroke?: ApexStroke;
+  grid?: ApexGrid;
+  tooltip?: ApexTooltip;
+  legend?: ApexLegend;
+};
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
-  
-  imports: [IconModule,MenuModule,NgApexchartsModule],
+  imports: [IconModule, MenuModule, NgApexchartsModule, NgScrollbarModule,CommonModule,FormsModule, NgSelectModule],
   animations: [
     trigger('toggleAnimation', [
-        transition(':enter', [style({ opacity: 0, transform: 'scale(0.95)' }), animate('100ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))]),
-        transition(':leave', [animate('75ms', style({ opacity: 0, transform: 'scale(0.95)' }))]),
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.95)' }),
+        animate('100ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
+      ]),
+      transition(':leave', [
+        animate('75ms', style({ opacity: 0, transform: 'scale(0.95)' })),
+      ]),
     ]),
-],
+  ],
   templateUrl: './analytics.component.html',
-  styleUrl: './analytics.component.css'
+  styleUrls: ['./analytics.component.css']
 })
-export class AnalyticsComponent {
-  totalVisit: any;
-    paidVisit: any;
-    uniqueVisitor: any;
-    followers: any;
-    referral: any;
-    engagement: any;
-    isLoading = true;
-    initCharts() {
-      // const isDark = this.store.theme === 'dark' || this.store.isDarkMode ? true : false;
-      // const isRtl = this.store.rtlClass === 'rtl' ? true : false;
+export class AnalyticsComponent implements OnInit {
+  private apiUrl = baseURL.apiUrl;
+  userOptions: { label: string; value: number }[] = [];
+  topReceivers: any[] = [];
+  topSenders: any[] = [];
+  fileUploadStats: any[] = [];
+  selectedUserId: number | null = null;
+  donutChart: any;
+  mostActiveFilesChartOptions: ChartOptions = {
+    series: [],
+    chart: { type: 'bar' },
+    xaxis: { categories: [] },
+    title: { text: '' }
+  };
 
-      // statistics
-      this.totalVisit = {
-          chart: {
-              height: 58,
-              type: 'line',
-              fontFamily: 'Nunito, sans-serif',
-              sparkline: {
-                  enabled: true,
-              },
-              dropShadow: {
-                  enabled: true,
-                  blur: 3,
-                  color: '#009688',
-                  opacity: 0.4,
-              },
-          },
-          stroke: {
-              curve: 'smooth',
-              width: 2,
-          },
-          colors: ['#009688'],
-          grid: {
-              padding: {
-                  top: 5,
-                  bottom: 5,
-                  left: 5,
-                  right: 5,
-              },
-          },
-          tooltip: {
-              x: {
-                  show: false,
-              },
-              y: {
-                  title: {
-                      formatter: (val: any) => {
-                          return '';
-                      },
-                  },
-              },
-          },
-          series: [
-              {
-                  data: [21, 9, 36, 12, 44, 25, 59, 41, 66, 25],
-              },
-          ],
-      };
+  constructor(private http: HttpClient) {}
 
-      //paid visit
-      this.paidVisit = {
-          chart: {
-              height: 58,
-              type: 'line',
-              fontFamily: 'Nunito, sans-serif',
-              sparkline: {
-                  enabled: true,
-              },
-              dropShadow: {
-                  enabled: true,
-                  blur: 3,
-                  color: '#e2a03f',
-                  opacity: 0.4,
-              },
-          },
-          stroke: {
-              curve: 'smooth',
-              width: 2,
-          },
-          colors: ['#e2a03f'],
-          grid: {
-              padding: {
-                  top: 5,
-                  bottom: 5,
-                  left: 5,
-                  right: 5,
-              },
-          },
-          tooltip: {
-              x: {
-                  show: false,
-              },
-              y: {
-                  title: {
-                      formatter: (val: any) => {
-                          return '';
-                      },
-                  },
-              },
-          },
-          series: [
-              {
-                  data: [22, 19, 30, 47, 32, 44, 34, 55, 41, 69],
-              },
-          ],
-      };
+  ngOnInit() {
+    this.http.get<any>(`${this.apiUrl}/Admin/dashboard-stats`).subscribe(data => {
+      this.userOptions = data.userLoginTimes.map((u: any) => ({
+        label: u.email,
+        value: u.userId,
+      }));
+         this.topReceivers = data.topReceivers;
+        this.topSenders = data.topSenders;
+        this.fileUploadStats = data.fileUploadStats;
+      this.initCharts(data);
+    });
+  }
+onUserSelect(userId: any) {
+    if (!userId) return;
+    console.log('Selected User ID:', userId);
 
-      // unique visitors
-      this.uniqueVisitor = {
-          chart: {
-              height: 360,
-              type: 'bar',
-              fontFamily: 'Nunito, sans-serif',
-              toolbar: {
-                  show: false,
-              },
-          },
-          dataLabels: {
-              enabled: false,
-          },
-          stroke: {
-              width: 2,
-              colors: ['transparent'],
-          },
-          colors: ['#5c1ac3', '#ffbb44'],
-          dropShadow: {
-              enabled: true,
-              blur: 3,
-              color: '#515365',
-              opacity: 0.4,
-          },
-          plotOptions: {
-              bar: {
-                  horizontal: false,
-                  columnWidth: '55%',
-                  borderRadius: 10,
-                  borderRadiusApplication: 'end',
-              },
-          },
-          legend: {
-              position: 'bottom',
-              horizontalAlign: 'center',
-              fontSize: '14px',
-              itemMargin: {
-                  horizontal: 8,
-                  vertical: 8,
-              },
-          },
-          grid: {
-              borderColor:   '#e0e6ed',
-              padding: {
-                  left: 20,
-                  right: 20,
-              },
-          },
-          xaxis: {
-              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-              axisBorder: {
-                  show: true,
-                  color:'#e0e6ed',
-              },
-          },
-          yaxis: {
-              tickAmount: 6,
-              opposite: false,
-              labels: {
-                  offsetX:0,
-              },
-          },
-          fill: {
-              type: 'gradient',
-              gradient: {
-                  shade:'light',
-                  type: 'vertical',
-                  shadeIntensity: 0.3,
-                  inverseColors: false,
-                  opacityFrom: 1,
-                  opacityTo: 0.8,
-                  stops: [0, 100],
-              },
-          },
-          tooltip: {
-              marker: {
-                  show: true,
-              },
-              y: {
-                  formatter: (val: any) => {
-                      return val;
-                  },
-              },
-          },
-          series: [
-              {
-                  name: 'Direct',
-                  data: [58, 44, 55, 57, 56, 61, 58, 63, 60, 66, 56, 63],
-              },
-              {
-                  name: 'Organic',
-                  data: [91, 76, 85, 101, 98, 87, 105, 91, 114, 94, 66, 70],
-              },
-          ],
-      };
-
-      // followers
-      this.followers = {
-          chart: {
-              height: 176,
-              type: 'area',
-              fontFamily: 'Nunito, sans-serif',
-              sparkline: {
-                  enabled: true,
-              },
-          },
-          stroke: {
-              curve: 'smooth',
-              width: 2,
-          },
-          colors: ['#4361ee'],
-          grid: {
-              padding: {
-                  top: 5,
-              },
-          },
-          yaxis: {
-              show: false,
-          },
-          tooltip: {
-              x: {
-                  show: false,
-              },
-              y: {
-                  title: {
-                      formatter: (val: any) => {
-                          return '';
-                      },
-                  },
-              },
-          },
-          fill: {
-                    type: 'gradient',
-                    gradient: {
-                        type: 'vertical',
-                        shadeIntensity: 1,
-                        inverseColors: !1,
-                        opacityFrom: 0.3,
-                        opacityTo: 0.05,
-                        stops: [100, 100],
-                    },
-                },
-          series: [
-              {
-                  data: [38, 60, 38, 52, 36, 40, 28],
-              },
-          ],
-      };
-
-      // referral
-      this.referral = {
-          chart: {
-              height: 176,
-              type: 'area',
-              fontFamily: 'Nunito, sans-serif',
-              sparkline: {
-                  enabled: true,
-              },
-          },
-          stroke: {
-              curve: 'smooth',
-              width: 2,
-          },
-          colors: ['#e7515a'],
-          grid: {
-              padding: {
-                  top: 5,
-              },
-          },
-          yaxis: {
-              show: false,
-          },
-          tooltip: {
-              x: {
-                  show: false,
-              },
-              y: {
-                  title: {
-                      formatter: (val: any) => {
-                          return '';
-                      },
-                  },
-              },
-          },
-          fill: {
-                    type: 'gradient',
-                    gradient: {
-                        type: 'vertical',
-                        shadeIntensity: 1,
-                        inverseColors: !1,
-                        opacityFrom: 0.3,
-                        opacityTo: 0.05,
-                        stops: [100, 100],
-                    },
-                },
-          series: [
-              {
-                  data: [60, 28, 52, 38, 40, 36, 38],
-              },
-          ],
-      };
-
-      // engagement
-      this.engagement = {
-          chart: {
-              height: 176,
-              type: 'area',
-              fontFamily: 'Nunito, sans-serif',
-              sparkline: {
-                  enabled: true,
-              },
-          },
-          stroke: {
-              curve: 'smooth',
-              width: 2,
-          },
-          colors: ['#1abc9c'],
-          grid: {
-              padding: {
-                  top: 5,
-              },
-          },
-          yaxis: {
-              show: false,
-          },
-          tooltip: {
-              x: {
-                  show: false,
-              },
-              y: {
-                  title: {
-                      formatter: (val: any) => {
-                          return '';
-                      },
-                  },
-              },
-          },
-          fill : {
-                    type: 'gradient',
-                    gradient: {
-                        type: 'vertical',
-                        shadeIntensity: 1,
-                        inverseColors: !1,
-                        opacityFrom: 0.3,
-                        opacityTo: 0.05,
-                        stops: [100, 100],
-                    },
-                },
-          series: [
-              {
-                  name: 'Sales',
-                  data: [28, 50, 36, 60, 38, 52, 38],
-              },
-          ],
-      };
+    this.http
+      .get<any>(`${this.apiUrl}/Admin/user-file-type-counts?userId=${userId.value}`)
+      .subscribe(data => {
+        this.updateDonutChart(data);
+      });
   }
 
+  updateDonutChart(data: any) {
+    const isDark = false; 
+
+    this.donutChart = {
+      series: [data.documents, data.excel, data.powerPoint],
+      chart: {
+        height: 300,
+        type: 'donut',
+        zoom: { enabled: false },
+        toolbar: { show: false }
+      },
+      labels: ['Documents', 'Excel', 'PowerPoint'],
+      colors: ['#5c6bc0', '#42a5f5', '#66bb6a'],
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+      },
+      legend: {
+        position: 'bottom',
+        labels: { colors: isDark ? '#fff' : '#000' }
+      }
+    };
+  }
+
+  initCharts(data: any) {
+  
+    // Most Active Files
+    this.mostActiveFilesChartOptions = {
+  series: [{
+    name: 'Shares',
+    data: data.mostActiveFiles.map((f: any) => f.shareCount)
+  }],
+  chart: {
+    type: 'bar',
+    height: 300,
+    zoom: { enabled: false },
+    toolbar: { show: false }
+  },
+  colors: [ '#e7515a' , '#f6b26b', '#f9cb9c', '#ffe599', '#93c47d', '#76a5af', '#6fa8dc', '#8e7cc3', '#c27ba0'],
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    show: true,
+    width: 1,
+    colors: ['transparent']
+  },
+  plotOptions: {
+    bar: {
+      distributed: true,
+      horizontal: false,
+      columnWidth: '35%'
+    }
+  },
+  grid: {
+    borderColor:'#e0e6ed'
+  },
+  xaxis: {
+    categories: data.mostActiveFiles.map((f: any) => f.fileName),
+    axisBorder: {
+      color: '#e0e6ed'
+    }
+  },
+  yaxis: {
+    labels: {
+      offsetX:  0
+    }
+  },
+  tooltip: {
+    theme: 'light',
+    y: {
+      formatter: (val: any) => val
+    }
+  },
+  title: { text: 'Most Shared Files' }
+    };
+  }
 }
